0a4887886e537231e57d5aed2c857c5d42b6be4d
miura <miura@qu-bic.jp>
2021-01-13 10:14:15 +0900

    first commit

---
+++ js/myscript.js
@@ -0,0 +1,76 @@
+
+'use strict';
+
+const w = $(window).width();
+const spwidth = 767;
+const tabletwidth = 1025;
+
+{
+	const openMenu = document.getElementById('menu_open');
+	const Nav = document.querySelector('header nav');
+	const body = document.querySelector('body');
+
+	openMenu.addEventListener('click', function () {
+		openMenu.classList.toggle('active');
+		Nav.classList.toggle('active');
+		body.classList.toggle('active');
+	});
+}
+
+
+$(function () {
+	$("body").removeClass("preload");
+	// JQueryの範囲
+
+	// ハンバーガーメニューのアニメーション発動用クラス付与
+	$('#menu_open').on('click',function(){
+		if($(this).hasClass('active')){
+			$(this).removeClass('close');
+			$('header nav').fadeIn(400);
+		}else{
+			$(this).addClass('close');
+			$('header nav').fadeOut(400);
+		}
+	});
+
+	// トップ：5秒ごとに「就活力UP!」「学びのページ」のテキスト切替
+	setInterval(function(){
+		$('.top01 .mv .links a').children('p').toggleClass('active');
+	}, 5000);
+
+	// テキストアニメーション発動
+	$('.txt_anime').on('inview',function(){
+		$(this).addClass('start');
+	});
+	// フェードイン
+	$('.fadein').on('inview',function(){
+		$(this).addClass('active');
+	});
+
+	// スムーススクロール
+	var headerHeight = $('header').outerHeight();
+	var urlHash = location.hash;
+	if(urlHash) {
+			$('body,html').stop().scrollTop(0);
+			setTimeout(function(){
+					var target = $(urlHash);
+					var position = target.offset().top - headerHeight;
+					$('body,html').stop().animate({scrollTop:position}, 500);
+			}, 100);
+	}
+	$('a[href^="#"]').click(function () {
+		var speed = 300;
+		if (w > spwidth) {
+			var header = 100;
+		} else {
+			var header = 0;
+		}
+		var href = $(this).attr("href");
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		var position = target.offset().top;
+		$("html, body").animate({ scrollTop: position - header }, speed, "swing");
+		return false;
+	}); 
+
+
+});
